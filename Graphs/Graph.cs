f696d96373603f14439fdb18bd61ee89468da6de
Phenomenalssss <frango.vasya@mail.ru>
2026-05-19 12:38:38 +0300

    Добавьте файлы проекта.

---
+++ Graphs/Graph.cs
@@ -0,0 +1,66 @@
+﻿namespace Graphs
+{
+    public static class Graph
+    {
+        private static void ColorPrint(object text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text.ToString());
+            Console.ResetColor();
+        }
+
+        public static void CreateAdjacencyMatrix(int vertexCount, int edgeCount)
+        {
+            vertexCount = Convert.ToInt32(Console.ReadLine());
+            edgeCount = Convert.ToInt32(Console.ReadLine());
+            // Создаем двумерный массив для матрицы смежности
+            int[,] graph = new int[vertexCount, vertexCount];
+            // Добавляем вершины в матрицу смежности
+            for (int i = 0; i < edgeCount; i++)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                a--;
+                b--;
+                graph[a, b] = 1;
+                graph[b, a] = 1;
+            }
+            // Выводим матрицу смежности
+            for (int i = 0; i < vertexCount; i++)
+            {
+                for (int j = 0; j < vertexCount; j++)
+                {
+                    Console.Write(" {0}", graph[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void CheckingForDisorientation(int[,] matrix)
+        {
+            bool flag = true;
+            for (int i = 0; i < matrix.GetLength(0) && flag; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) && flag; j++)
+                {
+                    if (matrix[i, j] != matrix[j, i])
+                    {
+                        flag = false;
+                    }
+                    if (matrix[i, i] != 0)
+                    {
+                        flag = false;
+                    }
+                }
+            }
+            if (flag)
+            {
+                ColorPrint("YES\n", ConsoleColor.Green);
+            }
+            else
+            {
+                ColorPrint("NO\n", ConsoleColor.Red);
+            }
+        }
+    }
+}
---
+++ ProgramGraphs/Program.cs
@@ -0,0 +1,117 @@
+﻿using Graphs;
+
+namespace ProgramGraphs
+{
+    public class Program
+    {
+        public static void ColorPrint(object text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text.ToString());
+            Console.ResetColor();
+        }
+
+        public static void RepeatMain()
+        {
+            Console.Write("Ещё раз? (2 - да, 1 - с очисткой консоли, 0 - нет)\n>> ");
+            int repeat = Convert.ToInt32(Console.ReadLine());
+            switch (repeat)
+            {
+                case 2:
+                    {
+                        Main();
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.Clear();
+                        Main();
+                        break;
+                    }
+                case 0:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        public static void MatrixPrint(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    ColorPrint($"{matrix[i, j]} ", ConsoleColor.Yellow);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Выберете задание, введя его номер:\n" +
+                    "1. Проверка матрицы на неориентированность\n" +
+                    "2. Полный граф\n" +
+                    "3. Рекурсивный алгоритм обхода графа в глубину\n" +
+                    "4. Проверить существование пути от A в B\n" +
+                    "5. Минимальный путь между двумя вершинами в неориентированном графе\n" +
+                    ">> ");
+                int exercise = Convert.ToInt32(Console.ReadLine());
+                switch (exercise)
+                {
+                    case 1:
+                        {
+                            Console.Write("Введите N = ");
+                            int N = Convert.ToInt32(Console.ReadLine());
+                            int[,] matrix = new int[N, N];
+                            for (int i = 0; i < N; i++)
+                            {
+                                Console.Write($"Введите {N} чисел через пробел {i + 1}-й строки матрицы: ");
+                                string[] numbers = Console.ReadLine().Split();
+                                for (int j = 0; j < numbers.Length; j++)
+                                {
+                                    matrix[i, j] = Convert.ToInt32(numbers[j]);
+                                }
+                            }
+                            Console.WriteLine("Матрица:");
+                            MatrixPrint(matrix);
+                            Graph.CheckingForDisorientation(matrix);
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+                    case 3:
+                        {
+                            break;
+                        }
+                    case 4:
+                        {
+                            break;
+                        }
+                    case 5:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+                RepeatMain();
+            }
+            catch (Exception e)
+            {
+                ColorPrint($"Ошибка: {e.Message}\n", ConsoleColor.Red);
+                RepeatMain();
+            }
+        }
+    }
+}
